8b2c3ffc8c7b3f9cf768da55ce6032a12ddf75aa
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-07-21 10:18:20 +0200

    Add Header component with daisy ui

---
+++ app/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='navbar'>
+      <div className='navbar-start'>LOGO</div>
+      <div className='navbar-end'>
+        <div className='px-3'>Blog</div>
+        <div className='px-3'>Concept</div>
+        <div className='px-3'>Connexion</div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import styles from "./page.module.css";
+"use client"
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1 className='font-bold text-pink-500 text-5xl'>HELLO WORLD !</h1>
+     <Link href="/">Home</Link>
+     <Link href="blog">Blog</Link>
     </main>
   )
 }
